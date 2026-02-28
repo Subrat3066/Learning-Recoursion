4553f8df6123a1332026f43826df16064b490215
Subrat3066 <{your_github_email}>
2026-02-28 18:29:04 +0530

    factorial

---
+++ Basics_day_01/factorial.java
@@ -0,0 +1,13 @@
+public class factorial{
+    public static int calcFact(int n)
+    {
+        if(n == 1 || n == 0)return 1;
+
+        
+
+        return n*calcFact(n-1);
+    }
+    public static void main(String[] args) {
+        System.out.print(calcFact(5));
+    }
+}
