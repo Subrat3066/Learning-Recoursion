c6c56e033c05c58380993dc4ba8c43355359519b
Subrat3066 <{your_github_email}>
2026-02-28 18:57:32 +0530

    fibonacci

--- Basics_day_01/factorial.java
+++ Basics_day_01/factorial.java
@@ -3,8 +3,6 @@ public class factorial{
     {
         if(n == 1 || n == 0)return 1;
 
-        
-
         return n*calcFact(n-1);
     }
     public static void main(String[] args) {
---
+++ Basics_day_01/fibonacci.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class fibonacci{
+    public static void fibo(int n , int a , int b , int c)
+    {
+        if(n == 2)return ;
+        c = a +b;
+        System.out.print(c+" ");
+        fibo(n -1 , b , c , c);
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n == 0 || n == 1)
+        {
+            System.out.print(0);
+            return;
+        }
+        if(n == 2)
+        {
+            System.out.print(0+" "+1);
+            return;
+        }
+        System.out.print(0+" "+1+" ");
+        fibo(n , 0 , 1 , 0);
+    }  
+}
